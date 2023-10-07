3919e4173875e92affee985cabfff1bb1938456b
Steven Gong <gonghaima@hotmail.com>
2023-10-07 20:06:04 +1100

    add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-   <p className='text-6xl text-green-500'>Hello AI SaaS</p>
-  )
+    <Button variant="destructive" size="lg">
+      Click Me
+    </Button>
+  );
 }
