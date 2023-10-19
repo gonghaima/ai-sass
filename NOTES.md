5874fcb2257d2d3d62b31036593a5162d17c6663
Steven Gong <gonghaima@hotmail.com>
2023-10-20 07:53:48 +1100

    sideBar - buttom part

--- NOTES.md
+++ NOTES.md
@@ -14,7 +14,5 @@ Following https://ui.shadcn.com/docs/installation/next
 - normal folder will come with url routes
 - name the folder with (), will omit current folder as path, but keep checking its child. e.g. (dashboard)/(routes)/dashboard/page.tsx, will be available via http://localhost:3000/dashboard
 
-
-
-53:35 video in progress
+1:01:34 video in progress
 CODE REF: /Users/stevengong/temp/tm8/ref/next13-ai-saas
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,9 +3,62 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Montserrat } from 'next/font/google';
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music,
+  Settings,
+  VideoIcon,
+} from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 const poppins = Montserrat({ weight: '600', subsets: ['latin'] });
+
+const routes = [
+  {
+    label: 'Dashboard',
+    icon: LayoutDashboard,
+    href: '/dashboard',
+    color: 'text-sky-500',
+  },
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: 'text-violet-500',
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: 'text-pink-700',
+    href: '/image',
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: 'text-orange-700',
+    href: '/video',
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: 'text-emerald-500',
+    href: '/music',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: 'text-green-700',
+    href: '/code',
+  },
+  {
+    label: 'Settings',
+    icon: Settings,
+    href: '/settings',
+  },
+];
 
 const SideBar = () => {
   return (
@@ -19,6 +72,22 @@ const SideBar = () => {
             Genius
           </h1>
         </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn('h-5 w-5 mr-3', route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
