045085e9720597ef3fa5b8cc979c9cee74519166
Steven Gong <gonghaima@hotmail.com>
2023-12-08 17:01:17 +1100

    subscription & mock

--- lib/mock/prismadb.ts
+++ lib/mock/prismadb.ts
@@ -3,12 +3,15 @@
 interface UserSubscription {
   userId: string;
   stripeCustomerId: string | null;
+  stripePriceId: number;
+  stripeCurrentPeriodEnd: Date;
 }
 
 interface MockPrismaClient {
   userSubscription: {
     findUnique: (params: {
       where: { userId: string };
+      select: any;
     }) => Promise<UserSubscription | null>;
   };
 }
@@ -19,10 +22,15 @@ const mockPrismaClient: MockPrismaClient = {
       // Implement your logic to retrieve user subscription
       // This is a mock, so you can customize it based on your needs
       const userId = where.userId;
+      const today = new Date();
+      const tomorrow = new Date(today);
+      tomorrow.setDate(today.getDate() + 1);
       // Mocked data for demonstration purposes
       return {
         userId,
         stripeCustomerId: 'acct_1NVm5xH86PaaZgk9',
+        stripePriceId: 132,
+        stripeCurrentPeriodEnd: tomorrow,
       };
     },
   },
